625fc8eb9e90dae7216781cf97de9c46535b5db9
Tomasz Makowski <50905772+MAKUSref@users.noreply.github.com>
2022-07-06 15:24:40 +0200

    Delete dist directory

--- public/index.js
+++ public/index.js
@@ -1,139 +1,141 @@
-const taskListEl = document.querySelector('.tasks-list');
-const taskFormEl = document.querySelector('.task-form');
-const titleInputEl = document.querySelector('#title');
-const descriptionInputEl = document.querySelector('#description');
-const removeLastEl = document.querySelector('.remove-last');
-const clearAllEl = document.querySelector('.clear-all');
-const addItemEl = document.querySelector('.add-item');
-const errorMsgEl = document.querySelector('.error-msg');
-
-const URL = 'http://localhost:3000';
-const API_URL = `${URL}/api`;
-
 window.addEventListener('DOMContentLoaded', () => {
+  const taskListEl = document.querySelector('.tasks-list');
+  const taskFormEl = document.querySelector('.task-form');
+  const titleInputEl = document.querySelector('#title');
+  const descriptionInputEl = document.querySelector('#description');
+  const removeLastEl = document.querySelector('.remove-last');
+  const clearAllEl = document.querySelector('.clear-all');
+  const addItemEl = document.querySelector('.add-item');
+  const errorMsgEl = document.querySelector('.error-msg');
+
+  const URL = 'http://localhost:3000';
+  const API_URL = `${URL}/api`;
+
   get(`${API_URL}/get`);
 
   taskFormEl.addEventListener('submit', (e) => {
     // Dodane tylko dlatego bo baza danych przechowywana jest w zmiennej
     e.preventDefault();
-  
+
     const title = titleInputEl.value;
     const description = descriptionInputEl.value;
-  
+
     const item = { title, description };
-  
+
     titleInputEl.value = '';
     descriptionInputEl.value = '';
-  
+
     add(`${API_URL}/add`, item);
   });
-  
+
   clearAllEl.addEventListener('click', () => {
     clear(`${API_URL}/clear`);
   });
-  
+
   removeLastEl.addEventListener('click', () => {
     remove(`${API_URL}/remove`);
   });
-});
 
-function add(url, item) {
-  return fetch(url, {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify(item)
-  })
-    .then((res) => res.json().then((data) => [data, res.status]))
-    .then((data) => {
-      const [databaseData, httpStatus] = data;
-      const { status, items } = databaseData;
 
-      if (httpStatus === 201) {
-        clearStatus();
-        addNewListItem(items[0]);
-      } else {
-        titleInputEl.value = item.title;
-        descriptionInputEl.value = item.description;
-        errorMsgEl.textContent = status;
-      }
-    });
-}
-
-function get(url) {
-  return fetch(url)
-    .then((res) => res.json().then((data) => [data, res.status]))
-    .then((data) => {
-      const [databaseData, httpStatus] = data;
-      const { items } = databaseData;
-
-      if (httpStatus === 200) {
-        clearStatus();
-        renderList(items);
-      }
-    });
-}
-
-function clear(url) {
-  return fetch(url, { method: 'PUT' })
-    .then((res) => res.json().then((data) => [data, res.status]))
-    .then((data) => {
-      const [databaseData, httpStatus] = data;
-
-      if (httpStatus === 200) {
-        clearStatus();
-        taskListEl.innerHTML = '';
-      }
-    });
-}
-
-function remove(url) {
-  return fetch(url, {
-    method: 'DELETE'
-  })
-    .then((res) => res.json().then((data) => [data, res.status]))
-    .then((data) => {
-      const [databaseData, httpStatus] = data;
-      const { status } = databaseData;
-
-      if (httpStatus === 200) {
-        clearStatus();
-        taskListEl.removeChild(taskListEl.lastChild);
-      } else {
-        errorMsgEl.textContent = status;
-      }
+  function add(url, item) {
+    return fetch(url, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(item)
     })
-}
+      .then((res) => res.json().then((data) => [data, res.status]))
+      .then((data) => {
+        const [databaseData, httpStatus] = data;
+        const { status, items } = databaseData;
 
-function renderList(list) {
-  taskListEl.innerHTML = '';
-  clearStatus();
+        if (httpStatus === 201) {
+          clearStatus();
+          addNewListItem(items[0]);
+        } else {
+          titleInputEl.value = item.title;
+          descriptionInputEl.value = item.description;
+          errorMsgEl.textContent = status;
+        }
+      });
+  }
 
-  list.forEach((task) => {
-    addNewListItem(task);
-  });
-}
+  function get(url) {
+    return fetch(url)
+      .then((res) => res.json().then((data) => [data, res.status]))
+      .then((data) => {
+        const [databaseData, httpStatus] = data;
+        const { items } = databaseData;
 
-function clearStatus() {
-  errorMsgEl.textContent = '';
-}
+        if (httpStatus === 200) {
+          clearStatus();
+          renderList(items);
+        }
+      });
+  }
 
-function addNewListItem(item) {
-  const newEl = createListItem(item.title, item.description);
-  taskListEl.appendChild(newEl);
-}
+  function clear(url) {
+    return fetch(url, { method: 'PUT' })
+      .then((res) => res.json().then((data) => [data, res.status]))
+      .then((data) => {
+        const [databaseData, httpStatus] = data;
 
-function createListItem(title, desc) {
-  const listItemEl = document.createElement('li');
-  const titleEl = document.createElement('p');
-  titleEl.classList.add('item-title');
-  const descEl = document.createElement('p');
-  descEl.classList.add('item-desc');
+        if (httpStatus === 200) {
+          clearStatus();
+          taskListEl.innerHTML = '';
+        }
+      });
+  }
 
-  titleEl.textContent = title;
-  descEl.textContent = desc;
+  function remove(url) {
+    return fetch(url, {
+      method: 'DELETE'
+    })
+      .then((res) => res.json().then((data) => [data, res.status]))
+      .then((data) => {
+        const [databaseData, httpStatus] = data;
+        const { status } = databaseData;
 
-  listItemEl.appendChild(titleEl);
-  listItemEl.appendChild(descEl);
+        if (httpStatus === 200) {
+          clearStatus();
+          taskListEl.removeChild(taskListEl.lastChild);
+        } else {
+          errorMsgEl.textContent = status;
+        }
+      })
+  }
 
-  return listItemEl;
-}
+  function renderList(list) {
+    taskListEl.innerHTML = '';
+    clearStatus();
+
+    list.forEach((task) => {
+      addNewListItem(task);
+    });
+  }
+
+  function clearStatus() {
+    errorMsgEl.textContent = '';
+  }
+
+  function addNewListItem(item) {
+    const newEl = createListItem(item.title, item.description);
+    taskListEl.appendChild(newEl);
+  }
+
+  function createListItem(title, desc) {
+    const listItemEl = document.createElement('li');
+    const titleEl = document.createElement('p');
+    titleEl.classList.add('item-title');
+    const descEl = document.createElement('p');
+    descEl.classList.add('item-desc');
+
+    titleEl.textContent = title;
+    descEl.textContent = desc;
+
+    listItemEl.appendChild(titleEl);
+    listItemEl.appendChild(descEl);
+
+    return listItemEl;
+  }
+
+});
